e927bea6184ea7d87f39b822da0a3c307278a826
Mark-Vale <markpvale@gmail.com>
2020-10-21 11:03:46 -0400

    add html popup

--- map1.py
+++ map1.py
@@ -6,6 +6,11 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+html = """
+<h4>Volcano information:</h4> 
+Height: %s m
+"""
+
 def color_producer(elevation):
     if elevation < 1000:
         return 'green'
@@ -19,7 +24,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=3, tiles="Stamen Terrain")
 fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 8, popup=str(el) + " meters",
+    iframe = folium.IFrame(html = html % str(el), width = 200, height = 100)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 8, popup=folium.Popup(iframe),
     parse_html=True, fill=True, fill_color=color_producer(el), color = "gray", 
     fill_opacity=0.7))
 
@@ -29,9 +35,12 @@ fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='UTF-8-sig').
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 1000000 else
 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fgv)
 map.add_child(fgp)
+map.add_child(fgv)
+
 map.add_child(folium.LayerControl())
 
-map.save("Map1.html")
+map.save("Map_html_popup_advanced.html")
+
+
 
